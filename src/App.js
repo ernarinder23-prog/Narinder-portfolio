3dd6e9e43e5ec43462897b6fe263f51e5787a953
navi263 <desibox033@gmail.com>
2026-03-24 13:39:54 +0530

    initial stage

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { HelmetProvider } from 'react-helmet-async';
+import './App.css'
+import SEO from './components/seo'
+import Home from './components/home/Home'
+import About from './components/about/About'
+import Skills from './components/skills/Skills'
+import Experience from './components/experience/Experience'
+import Projects from './components/projects/Projects'
+import Contact from './components/contact/Contact'
+import Footer from './components/footer/Footer'
+import TopButton from './components/topButton/TopButton'
+
+function App() {
+  return (
+    <HelmetProvider>
+      <div className="App">
+        <SEO />
+        <Home />
+        <About />
+        <Skills />
+        <Experience />
+        <Projects />
+        <Contact />
+        <Footer />
+        <TopButton />
+      </div>
+    </HelmetProvider>
+  )
+}
+
+export default App
