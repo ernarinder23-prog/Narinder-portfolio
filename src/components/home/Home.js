d8d177a60d94df913e76ae8f745b23ff3585fca0
ernarinder23-prog <er.narinder23@gmail.com>
2026-05-16 16:52:27 +0530

    experience changes

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -85,7 +85,7 @@ const Home = () => {
             <Bounce cascade>
               <div className="resume-container">
                 <a
-                  href="https://drive.google.com/file/d/1rqDhErYyen7JXE3SkK4Ip0ImvXGJOeT8/view?usp=sharing"
+                  href="https://drive.google.com/file/d/1ghF1MwWedLfOzco095Mhd0wENsvujIke/view?usp=sharing"
                   target="_blank"
                   rel="noopener noreferrer"
                   aria-label="Download Narinder Singh's resume"
